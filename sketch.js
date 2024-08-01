f10c3e6eaa97cba0c4fff574ea994b8534a042da
P.Arthur <00001233422595SP@al.educacao.sp.gov.br>
2024-08-01 18:27:44 -0300

    alteraçao total

--- sketch.js
+++ sketch.js
@@ -1,18 +1,15 @@
 function setup() {
-    createCanvas(600, 600);
-     background("white");
+  createCanvas(600, 600);
+  background("white");
+}
+
+function draw() {
+  stroke("(black)");
+  fill("purple");
+
+  // console.log(mouseIsPressed)
+
+  if (mouseIsPressed) {
+    rect(mouseX, mouseY, 20, 35);
   }
-  
-  
-  function draw() {
-    
-    stroke ("#795548");
-    fill ("#3F51B5");
-   
-    
-    // console.log(mouseIsPressed);
-    
-    if (mouseIsPressed){
-      rect(mouseX, mouseY, 20, 35);
-    }
-  }
+}
